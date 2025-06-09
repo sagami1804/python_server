d7acdc593399a2a8e42c946e0dcea33b17ec151f
shoji <sagami8350@gmail.com>
2025-06-09 18:37:21 +0900

    rsa_all6

--- client.py
+++ client.py
@@ -4,12 +4,14 @@ from rsa import *
 
 SERVER_IP = '10.0.3.85'
 PORT = 12345
+
 # 秘密鍵
 P = 997
 Q = 859
 N = P * Q
 E = 65537
 
+# 暗号化
 def encryption(text):
     code = encRsaCrt(text,N,E)
     return code
@@ -17,14 +19,23 @@ def encryption(text):
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         
+        # サーバーに接続
         s.connect((SERVER_IP, PORT))
         print("接続しました。メッセージを送信してください")
         
         while True:
             msg = input(f"4桁の数字を-で区切って入力して下さい（終了するには空Enter）: ")
+            
+            # 空Enterで停止
+            if str(msg) == "":
+                break
+
+            # 入力した文字列をハイフンで4分割
             msg = msg.split('-')
             enc_msg = ''
             count = 0
+            
+            # 4桁ずつ暗号化
             for numbers in msg:
                 if count == 3:
                     enc_msg = enc_msg + str(encryption(int(numbers)))
@@ -33,11 +44,11 @@ def main():
                 count = count + 1
 
             print("暗号:"+str(enc_msg))
-            if str(msg) == "":
-                break
-            if str(msg) == "exit":
-                break
+            
+            # サーバーに送信
             s.sendall(enc_msg.encode())
+            
+            # サーバーから受信(デバッグ)
             data = s.recv(1024)
             print("サーバーからの返信:", data.decode())
         
--- server.py
+++ server.py
@@ -20,17 +20,25 @@ DP = D % (P - 1)
 DQ = D % (Q - 1)
 QINV = modinv(Q, P)
 
+# クライアントとの通信
 def handle_client(conn, addr):
     print(f"{addr} と接続されました")
     with conn:
         while True:
+            # データ受信
             data = conn.recv(1024)
+            
+            # データが空なら終了
             if not data:
                 break
             data = data.decode()
             print(f"{addr} から受信(暗号): {data}")
+            
+            # ハイフンで4分割
             data_list = data.split('-')
             dec_data = ''
+            
+            # 4桁ずつ復号
             for i in range(4):
                 if i == 3:
                     dec_data = dec_data + str(decryption(int(data_list[i])))
@@ -38,12 +46,13 @@ def handle_client(conn, addr):
                     dec_data = dec_data + str(decryption(int(data_list[i]))) + '-'
             print(f"{addr} から受信(平文): {dec_data}\n")
             
-            if data == "exit":
-                break
+            # 復号結果をクライアントに送信
             response = "受信しました: " + dec_data
             conn.sendall(response.encode())
+            
     print(f"{addr} との接続を終了しました")
 
+# 復号
 def decryption(code):
     text = ""
     text = decRsaCRT(code,N,P,Q,DP,DQ,QINV)
@@ -51,13 +60,17 @@ def decryption(code):
     
     
 def main():
+    # サーバー起動
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
         print(f"サーバー起動中... ポート {PORT} で待機中")
 
+        # クライアントと通信
         while True:
             conn, addr = s.accept()
+            
+            # 非同期で1対多通信
             thread = threading.Thread(target=handle_client, args=(conn, addr))
             thread.start()
         

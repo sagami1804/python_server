da7ab91c3489b2a5e3ce0afed2d8a792cb287a67
sagami1804 <61611865+sagami1804@users.noreply.github.com>
2025-06-15 21:35:01 +0900

    時間計測を削除

--- server.py
+++ server.py
@@ -38,19 +38,9 @@ def handle_client(conn, addr):
                 break
             data = data.decode()
             print(f"\n{addr} から受信(暗号): {data}\n")
-            
-            start = time.perf_counter() #計測開始
             dec_data = str(decryption(int(data)))
-            end = time.perf_counter() #計測終了
-            print(f"復号にかかった時間(CRT)　　　: {end - start:.6f}秒")
             print(f"{addr} から受信(平文): {dec_data}\n")
-            
-            start = time.perf_counter() #計測開始
-            dec_data = str(decRsa(int(data), N, D))
-            end = time.perf_counter() #計測終了
-            print(f"復号にかかった時間(通常のRSA): {end - start:.6f}秒")
-            print(f"{addr} から受信(平文): {dec_data}\n")
-            
+
             # 復号結果をクライアントに送信
             dec_data_add_hyphen = f"{dec_data[:4]}-{dec_data[4:8]}-{dec_data[8:12]}-{dec_data[12:]}"
             response = "受信しました: " + dec_data_add_hyphen
